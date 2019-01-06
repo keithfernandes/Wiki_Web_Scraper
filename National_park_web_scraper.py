9557098df07468c777a7938d2a2213fe9ba0fe7a
keith fernandes <fernandeskeith9@gmail.com>
2019-01-06 13:51:49 -0500

    Almost clean dataset created

--- National_park_web_scraper.py
+++ National_park_web_scraper.py
@@ -10,10 +10,12 @@ Created on Sat Jan  5 15:29:21 2019
 from bs4 import BeautifulSoup
 import urllib.request
 import csv
+import pandas as pd
+import re
 
 
 filename = "nationalpark.csv"
-f = open(filename,"w")
+f = open(filename,"w",encoding='utf-8-sig')
 
 
 
@@ -27,20 +29,46 @@ page_soup = BeautifulSoup(page,'html.parser')
     
 parks_table = page_soup.find("table")
 rows = parks_table.findAll("tr")
-header_part = parks_table.find("thead")
 
-headers = header_part.findAll("th")
-for header in headers:
-    print(header.text.strip()+"\n")
 
-'''
-for tr in rows:
-    cols = tr.findAll("td")
-    for td in cols:
-        print(td.text.strip())
-    print('\n')   
+# Write the row header to the csv file
+f.write("Park_Name"+","+"Image_Url"+","+"Latitude"+","+"Longitude"+","+"Date_Established"+","+"Area"+","+"No_of_Visitors"+","+"Description"+"\n")
+
+
+for rowindex,tr in enumerate(rows[1:]):
+        
+            cols = tr.find_all(["th","td"])
+                
+            
+            
+            for index,td in enumerate(cols):
+             
+                try:
+                            
+# For the last table element write to file with newline                 
+                     if(index == 6):
+                         f.write(td.text.replace(',','').rstrip()+'\n')
+                     else:
+                         if(index == 2):      
+# extract location from col at index 2 and split extracted location to latitude and longitude
+                          latlong = re.findall(r"[-+]?\d*\.\d+|\d+", td.text.replace(',','').rstrip())
+                          print(latlong)
+                          f.write(latlong[-2]+','+latlong[-1]+',')
+                          print(latlong)
+                         else: 
+                          f.write(td.text.replace(',','').rstrip()+',')
+                     
+                except Exception as ex:
+                    
+                     template = "An exception of type {0} occurred. Arguments:\n{1!r}"
+                     message = template.format(type(ex).__name__, ex.args)
+                     print(message)
+           
+               
+             
+          
     
- '''
+
 
 
 
@@ -59,4 +87,17 @@ for tr in rows:
 f.close()  
  
 page.close()
+
+
+df= pd.read_csv("nationalpark.csv")
+print(df.shape)
+
+
+
+
+
+
+
+
+
 
